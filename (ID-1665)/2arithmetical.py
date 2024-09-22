b49990a49963077f00def0a23c1c4ad498a8355f
Ramesha Rawnok Haque <142678206+rawnok-18@users.noreply.github.com>
2024-09-23 01:05:23 +0600

    Create 2arithmetical.py

---
+++ (ID-1665)/2arithmetical.py
@@ -0,0 +1,10 @@
+r= float(input("Enter the first number: "))
+s= float(input("Enter the second number: "))
+sum = r+s
+sub = r-s
+mul = r*s
+div = r/s
+print("The sum of 2 numbers: ",sum)
+print("The sub of 2 numbers: ",sub)
+print("The mul of 2 numbers: ",mul)
+print("The div of 2 numbers: ",div)
