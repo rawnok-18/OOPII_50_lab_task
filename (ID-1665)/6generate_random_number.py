9aff15be477cd0a1a92b36cb58ce2818c278c6c9
Ramesha Rawnok Haque <142678206+rawnok-18@users.noreply.github.com>
2024-09-23 01:54:34 +0600

    Create 6generate_random_number.py

---
+++ (ID-1665)/6generate_random_number.py
@@ -0,0 +1,7 @@
+import random
+
+n = random.randint(0, 98)
+
+print("Random Number between 0 to 98:", n)
+print("Random Number between 15 to 52:", random.randint(15, 52))
+print("Random Number between 56 to 98:", random.randint(56, 98))
