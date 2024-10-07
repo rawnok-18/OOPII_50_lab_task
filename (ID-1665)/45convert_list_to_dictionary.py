99f4b3065b1125ac58a62add6e705114aa775fab
Ramesha Rawnok Haque <142678206+rawnok-18@users.noreply.github.com>
2024-10-07 21:01:45 +0600

    Create 45convert_list_to_dictionary.py

---
+++ (ID-1665)/45convert_list_to_dictionary.py
@@ -0,0 +1,9 @@
+sharingan_users = ["kakashi", "itachi", "madara"]
+
+# Initialize an empty dictionary
+sharingan_users_dict = {}
+
+for idx, users in enumerate(sharingan_users):
+    sharingan_users_dict[idx] = users
+
+print(sharingan_users_dict)
